12bf5cfecc823b6679f81e12671836e5cb804fbb
Davide Rizzi <d.rizzi@novalabs.io>
2016-09-06 13:59:00 +0200

    Added new FindNovaCore_*

---
+++ FindNovaCore_MW.cmake
@@ -0,0 +1,91 @@
+IF( NOT NOVA_CORE_MW_ROOT )
+  IF( NOT NOVA_ROOT )
+    SET( NOVA_CORE_MW_ROOT /opt/nova/middleware )
+  ELSE()
+    SET( NOVA_CORE_MW_ROOT ${NOVA_ROOT}/core-mw )
+  ENDIF()
+
+  MESSAGE( STATUS "No NOVA_CORE_MW_ROOT specified, using default: ${NOVA_CORE_MW_ROOT}" )
+ENDIF()
+
+SET( NOVA_CORE_MW_SOURCES
+  ${NOVA_CORE_MW_ROOT}/src/core-mw.cpp
+  
+  ${NOVA_CORE_MW_ROOT}/src/impl/ArrayQueue_.cpp
+  ${NOVA_CORE_MW_ROOT}/src/impl/SimplePool_.cpp
+  ${NOVA_CORE_MW_ROOT}/src/impl/StaticList_.cpp
+  ${NOVA_CORE_MW_ROOT}/src/impl/StaticQueue_.cpp
+  ${NOVA_CORE_MW_ROOT}/src/BasePublisher.cpp
+  ${NOVA_CORE_MW_ROOT}/src/BaseSubscriber.cpp
+  ${NOVA_CORE_MW_ROOT}/src/BaseSubscriberQueue.cpp
+  ${NOVA_CORE_MW_ROOT}/src/BootMsg.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Checksummer.cpp
+  ${NOVA_CORE_MW_ROOT}/src/LocalPublisher.cpp
+  ${NOVA_CORE_MW_ROOT}/src/LocalSubscriber.cpp
+  ${NOVA_CORE_MW_ROOT}/src/MessagePtrQueue.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Message.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Middleware.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Node.cpp
+  ${NOVA_CORE_MW_ROOT}/src/RemotePublisher.cpp
+  ${NOVA_CORE_MW_ROOT}/src/RemoteSubscriber.cpp
+  ${NOVA_CORE_MW_ROOT}/src/TimestampedMsgPtrQueue.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Topic.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Transport.cpp
+  ${NOVA_CORE_MW_ROOT}/src/Utils.cpp
+  
+  ${NOVA_CORE_MW_ROOT}/src/CoreNode.cpp
+  ${NOVA_CORE_MW_ROOT}/src/CoreNodeManager.cpp
+  ${NOVA_CORE_MW_ROOT}/src/CoreConfiguration.cpp
+  ${NOVA_CORE_MW_ROOT}/src/CoreModule.cpp
+  
+  ${NOVA_CORE_MW_ROOT}/src/UID.cpp
+
+  ${NOVA_CORE_MW_ROOT}/port/chibios/src/impl/Middleware_.cpp
+)
+
+SET( NOVA_CORE_MW_INCLUDE_DIRS
+  ${NOVA_CORE_MW_ROOT}/include
+  ${NOVA_CORE_MW_ROOT}/port/chibios/include
+)
+
+IF( CORE_USE_RTCANTRANSPORT )
+  SET( TRANSPORT_SOURCES
+    ${NOVA_CORE_MW_ROOT}/src/transport/RTCANTransport.cpp
+    ${NOVA_CORE_MW_ROOT}/src/transport/RTCANPublisher.cpp
+    ${NOVA_CORE_MW_ROOT}/src/transport/RTCANSubscriber.cpp
+  )
+  
+  LIST( APPEND NOVA_CORE_MW_SOURCES ${TRANSPORT_SOURCES} )
+ENDIF()
+
+IF( CORE_USE_DEBUGTRANSPORT )
+  SET( TRANSPORT_SOURCES
+    ${NOVA_CORE_MW_ROOT}/src/transport/DebugTransport.cpp
+    ${NOVA_CORE_MW_ROOT}/src/transport/DebugPublisher.cpp
+    ${NOVA_CORE_MW_ROOT}/src/transport/DebugSubscriber.cpp
+  )
+  
+  LIST( APPEND NOVA_CORE_MW_SOURCES ${TRANSPORT_SOURCES} )
+ENDIF()
+
+IF( CORE_USE_BOOTLOADER )
+  SET( BOOTLOADER_SOURCES
+    ${NOVA_CORE_MW_ROOT}/src/Bootloader.cpp
+
+    ${NOVA_CORE_MW_ROOT}/port/chibios/src/impl/Bootloader_.cpp
+    ${NOVA_CORE_MW_ROOT}/port/chibios/src/impl/Flasher_.cpp
+  )
+  
+  LIST( APPEND NOVA_CORE_MW_SOURCES ${BOOTLOADER_SOURCES} )
+ENDIF()
+
+IF( NOVA_CORE_MW_SOURCES )
+  LIST( REMOVE_DUPLICATES NOVA_CORE_MW_SOURCES )
+ENDIF()
+
+IF( NOVA_CORE_MW_INCLUDE_DIRS )
+  LIST( REMOVE_DUPLICATES NOVA_CORE_MW_INCLUDE_DIRS )
+ENDIF()
+
+INCLUDE( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( NovaCore_MW DEFAULT_MSG NOVA_CORE_MW_SOURCES NOVA_CORE_MW_INCLUDE_DIRS )
